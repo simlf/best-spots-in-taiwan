3a4ec3e0e2f21db4a819ca18d0a05112429a5830
Simon LeFourn <lefournsimon@gmail.com>
2020-04-22 10:06:18 +0200

    added unique slugs

--- delicious-restaurant/controllers/storeController.js
+++ delicious-restaurant/controllers/storeController.js
@@ -1,13 +1,54 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    // Check that the user uploads an image (not something else)
+    // The mimetype gives the type of the file and its extension
+    // Example :
+    // mimetype: 'image/jpeg'
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({message: 'That file type isn\'t allowed'}, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   res.render('index', {title: 'Home'});
 };
 
 exports.addStore = (req, res) => {
-  res.render('editStore', {title: 'Add Store'});
-}
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.upload = multer(multerOptions).single('photo'); // uploaded to our filesystem but not saved yet in the DB
+
+exports.resize = async (req, res, next) => {
+  if (!req.file) {
+    next();  // skip it if there is no new file to resize
+    return;
+  };
+
+  // Rename the photo with a unique ID (uuid)
+  // then send it to the body request with it's mimetype extention (we don't trust the users extension)
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`
+
+  // Resize the photo
+  const photo = await jimp.read(req.file.buffer); // buffer is an area of memory where the photo is stored as a raw binary data
+  await photo.resize(800, jimp.AUTO); // width of 800 and automatic height
+  await photo.write(`./public/uploads/${req.body.photo}`); // writes the photo to our filesystem
+
+  // Go the next function
+  next();
+};
 
 // You should use async every you are dealing with the DB
 exports.createStore = async (req, res) => {
@@ -20,22 +61,25 @@ exports.getStores = async (req, res)  => {
   // Query DB for a list of all stores
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
-}
+};
 
 exports.editStore = async (req, res) => {
   // Find the store by its ID
   const store = await Store.findOne({_id: req.params.id});
+
   // Check if the user is the owner
   // LATER
 
   // Render out the edit form
   res.render('editStore', { title: `Edit ${store.name}`, store });
-}
+};
 
 exports.updateStore = async (req, res) => {
   // Find the store store and update it
   // findOneAndUpdate() is a method in MongoDB
   // it takes three parameters : query, data, options
+
+  req.body.location.type = 'Point';// set the location data to be a Point
   const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
       new: true, // return the new store instead of the former one
       runValidators: true, // run all the required validators from the models
@@ -44,4 +88,12 @@ exports.updateStore = async (req, res) => {
   // Redirect to the store and flash that it worked
   req.flash('success', `<strong>${store.name}</strong> was succesfully updated. <a href='/stores/${store.slug}'> View Store -> </a>`);
   res.redirect(`/stores/${store.id}/edit`);
-}
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  // Find the store in the DB
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  // Render the page
+  res.render('store', { store, title: store.name });
+};
--- delicious-restaurant/models/Store.js
+++ delicious-restaurant/models/Store.js
@@ -31,15 +31,25 @@ const storeSchema = new mongoose.Schema({
       type: String,
       required: 'Please enter an address!'
     }
-  }
+  },
+  photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop this function from running
   }
   this.slug = slug(this.name);
+
+  // Find if there are already Stores with the same slug
+  const slugRegEx = new RegExp (`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx});
+  // If there is already stores with the same slug, then this Store slug should be different
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length - 1}`;
+  }
+
   next();
 });
 
