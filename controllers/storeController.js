9379cb78f867fff7eb8dcc8a55c299d1236abcfc
Simon LeFourn <lefournsimon@gmail.com>
2020-04-23 09:00:04 +0200

    added user controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -97,3 +97,12 @@ exports.getStoreBySlug = async (req, res, next) => {
   // Render the page
   res.render('store', { store, title: store.name });
 };
+
+exports.getStoresByTag = async (req, res) => {
+  const tag = req.params.tag
+  const tagQuery = tag || { $exists: true }
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  res.render('tag', { tags, title: 'Tags', stores, tag });
+}
